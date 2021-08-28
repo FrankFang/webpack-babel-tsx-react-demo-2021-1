b9ec08b50b0643ab6b966c060ecce1a62e9e9975
frankfang <frankfang1990@gmail.com>
2021-08-29 00:52:48 +0800

    add example

--- example/example.tsx
+++ example/example.tsx
@@ -1,13 +1,31 @@
 import * as ReactDOM from 'react-dom'
-import {FButton} from '../lib/index';
+import { FButton } from '../lib/index';
+import { Router, Switch, Route } from 'react-router'
+import { createHashHistory } from 'history'
 
 const root = document.createElement('div')
 document.body.append(root)
+const history = createHashHistory()
+const Page = () => {
+  return (
+    <Router history={history}>
+      <div>导航栏</div>
+      <Switch>
+        <Route exact path="/">
+          <App />
+        </Route>
+        <Route>
+          <div>404</div>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
 
-const App = ()=>{
+const App = () => {
   return <div>
     <FButton>hi</FButton>
   </div>
 }
 
-ReactDOM.render(<App />, root)
+ReactDOM.render(<Page />, root)
